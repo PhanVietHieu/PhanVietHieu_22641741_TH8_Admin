aa9d7ce6d15f399010fef26c06dce8d2eccc5cd2
Phan Viết Hiếu <22641741.hieu@student.iuh.edu.vn>
2025-04-17 15:29:04 +0700

    second commit: get api overview

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,17 @@ import { useEffect, useState } from 'react';
 import '../css/admin.css'
 import DataTable from 'react-data-table-component';
 export default function Dashboard() {
+    const [cards, setCards] = useState([]);
+    const [data, setData] = useState([]);
+    const [selectedOrder, setSelectedOrder] = useState(null);
+    const [modalOpen, setModalOpen] = useState(false);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/overview")
+            .then((res) => res.json())
+            .then((data) => setCards(data))
+            .catch((err) => console.error("Error fetching data:", err));
+    }, []);
     return (
         <>
 
@@ -24,7 +35,26 @@ export default function Dashboard() {
                     <a href="#">Messages</a>
                     <a href="#">Integrations</a>
                 </div>
-                <div className="content"></div>
+                <div className="content">
+                    <div className="secA">
+                        <h2 className="overview-title">
+                            <span className="icon-square">▧</span> Overview
+                        </h2>
+                        <div className="overview-row">
+                            {cards.map((card) => (
+                                <div key={card.id} className="card-box">
+                                    <div className="card-title">{card.title}</div>
+                                    <div className="card-value">{card.value}</div>
+                                    <div className="card-change">
+                                        ▲ {card.change} <span className="card-subtext">period of change</span>
+                                    </div>
+                                    <div className="card-icon">{card.icon}</div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="secB"></div>
+                </div>
                 <div className="footer"></div>
             </div>
         </>
